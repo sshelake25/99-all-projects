fa9be2de7c636fbea18731c5eb7e2d462ac24731
Surekha Shelake <shelakesurekha@gmail.com>
2022-04-06 16:33:38 +0530

    added home and not found routes

--- learnlytica/src/app/app-routing.module.ts
+++ learnlytica/src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { ListContactComponent } from './components/list-contact/list-contact.component';
 import { ListCourseComponent } from './components/list-course/list-course.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ServicesComponent } from './components/services/services.component';
 
 const routes: Routes = [
-
-  { path: 'course', component: ListCourseComponent },
-
-  { path: 'services', component: ServicesComponent },
-  { path: 'contact', component: ListContactComponent }
-
+    { path: '', component: HomeComponent },
+    { path: 'course', component: ListCourseComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'contact', component: ListContactComponent },
+    { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- learnlytica/src/app/app.module.ts
+++ learnlytica/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 
@@ -11,6 +11,19 @@ import { ListCourseComponent } from './components/list-course/list-course.compon
 import { CourseComponent } from './components/course/course.component';
 import { ServicesComponent } from './components/services/services.component';
 import { ListContactComponent } from './components/list-contact/list-contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+
+
+const myRoutes: Routes = [
+
+  { path: 'course', component: ListCourseComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ListContactComponent }
+
+];
 
 @NgModule({
   declarations: [
@@ -21,11 +34,16 @@ import { ListContactComponent } from './components/list-contact/list-contact.com
     ListCourseComponent,
     CourseComponent,
     ServicesComponent,
-    ListContactComponent
+    ListContactComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+     AppRoutingModule
+
+ //   RouterModule.forRoot(myRoutes) //{useHash: true}
+
   ],
   providers: [],
   bootstrap: [AppComponent]
