82ffde3e1304a897f81a0729a96a67a7a03ba758
Surekha Shelake <shelakesurekha@gmail.com>
2022-04-14 15:00:17 +0530

    added command line and package.json initaized

--- todo-node/app.js
+++ todo-node/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -5,13 +7,16 @@ const port = 3000;
 
 const webServer = http.createServer(
     (req, res) => {
-        console.log(req.headers);
-        console.log("========================");
 
+        console.log(process.env.BATCH)
+       // console.log(req.headers);
+        console.log("========================");
+        console.log('hello world')
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.setHeader('thbs-Type', 'angular');
         res.end('Hello World');
+        process.exit(1)
     }
 )
 
---
+++ todo-node/demo.js
@@ -0,0 +1,10 @@
+
+console.log(`taking command line arg for node js progam`);
+process.argv.forEach((ele, index, arr) => {
+    console.log(`${ele} --> ${index}`);
+})
+
+const args = process.argv.slice(2); //
+
+console.log(args)
+
