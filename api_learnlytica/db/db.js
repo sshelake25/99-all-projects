4895d4a6472a522ae8002a3da51641ba6ddde466
Surekha Shelake <shelakesurekha@gmail.com>
2022-05-10 12:05:55 +0530

    added create table query

--- api_learnlytica/db/db.js
+++ api_learnlytica/db/db.js
@@ -10,13 +10,27 @@ let mysqlConnect = function () {
         // password: process.env.DB_PASSWORD,
         // database: process.env.DB_NAME,
         // insecureAuth : true
-        host     : process.env.DB_HOST,
-        user     : process.env.DB_USER,
-        password : process.env.DB_PASSWORD, //should be yours 
-        database : process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD, //should be yours 
+        database: process.env.DB_NAME,
         port: process.env.DB_PORT,
-        insecureAuth : true
+        insecureAuth: true
     });
 };
+
+let createTable = `create table if not exists user_profile(
+    id int primary key auto_increment,
+    title varchar(255)not null,
+    completed tinyint(1) not null default 0
+)`;
+
+mysqlConnect().query(createTable, (error, results, fields) => {
+    if (error) throw error;
+});
+
+// mysqlConnect().query(insetData, (error, results, fields) => {
+//     if (error) throw error;
+// });
 
 module.exports.localConnect = mysqlConnect;
