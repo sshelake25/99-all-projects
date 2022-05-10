7da5bdd828ae08ffa2e8dba25f1200efc35509b2
Surekha Shelake <shelakesurekha@gmail.com>
2022-05-10 09:53:02 +0530

    added required changes in project

--- api_learnlytica/db/db.js
+++ api_learnlytica/db/db.js
@@ -5,10 +5,16 @@ require('dotenv').config();
 
 let mysqlConnect = function () {
     return mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        // host: process.env.DB_HOST,
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PASSWORD,
+        // database: process.env.DB_NAME,
+        // insecureAuth : true
+        host     : process.env.DB_HOST,
+        user     : process.env.DB_USER,
+        password : process.env.DB_PASSWORD, //should be yours 
+        database : process.env.DB_NAME,
+        port: process.env.DB_PORT,
         insecureAuth : true
     });
 };
