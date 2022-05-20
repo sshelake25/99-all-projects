a9583f563471cf3dd3129835410533c6c7dddb02
Surekha Shelake <shelakesurekha@gmail.com>
2022-05-20 09:04:03 +0530

    added api call with fetch api in class compoonent

--- react-final/src/pages/HooksPage.js
+++ react-final/src/pages/HooksPage.js
@@ -2,23 +2,73 @@
 
 import React, { useState } from 'react';
 
-export default function HooksPage() {
-    //16.8 + 
-    const [count, setCount] = useState(10);
-    const [age, setMyAge] = useState(20)
+export default class HooksPage extends React.Component {
+    constructor(props) {
+        super(props);
+        //initilization of state
+        //setState()
+        this.state = {
+            users: [],
+            error: null,
+            isLoaded: false
+        }
+    }
 
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
-            </button>
-            <br></br>
-            age-- {age}
-            <button onClick={() => setMyAge(age + 1)}>
-                Click me
-            </button>
-        </div>
-    )
+    // sending api call //life cycle hook
+    componentDidMount() {
+
+        fetch('https://reqres.in/api/users?page=1')
+            .then(res => res.json())
+            .then(
+                (success) => {
+                    //success.data --> array of object of user
+                    this.setState({
+                        isLoaded: true,
+                        users: success.data
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        error: error
+
+                    });
+                }
+            )
+
+    }
+
+
+    render() {
+        const { users, isLoaded, error } = this.state;
+
+        return (
+            <ul>
+                {users.map(item => (
+                    <li key={item.id}>
+                        {item.first_name} {item.last_name}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+
+
+    // //16.8 + 
+    // const [count, setCount] = useState(10);
+    // const [age, setMyAge] = useState(20)
+
+    // return (
+    //     // <div>
+    //     //     <p>You clicked {count} times</p>
+    //     //     <button onClick={() => setCount(count + 1)}>
+    //     //         Click me
+    //     //     </button>
+    //     //     <br></br>
+    //     //     age-- {age}
+    //     //     <button onClick={() => setMyAge(age + 1)}>
+    //     //         Click me
+    //     //     </button>
+    //     // </div>
+    // )
 
 }
